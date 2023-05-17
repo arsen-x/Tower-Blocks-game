f4b182861dc6edb53f386dbfa4f53a9de9d35996
Arsen <arsyess06@gmail.com>
2023-05-17 17:21:24 +0500

    Designed the view of game's menu

--- game_canvas.py
+++ game_canvas.py
@@ -8,9 +8,18 @@ pygame.display.set_caption("Tower Blocks")
 
 # declaring FPS rate (frames per second)
 FPS = 60
+game_state = "Menu"
+MENU_BAKCGROUND = pygame.image.load("Assets/menu_background.png")
+MENU_TITLE = pygame.image.load("Assets/title.png")
+START_BUTTON = pygame.image.load("Assets/start_button.png")
+LEADERBOARD_BUTTON = pygame.image.load("Assets/leaderboard_button.png")
 
 
 def game_window():
+    GAME_WINDOW.blit(MENU_BAKCGROUND, (0, 0))
+    GAME_WINDOW.blit(MENU_TITLE, (140, 90))
+    GAME_WINDOW.blit(START_BUTTON, (295, 230))
+    GAME_WINDOW.blit(LEADERBOARD_BUTTON, (215, 300))
     pygame.display.update()
 
 
@@ -23,8 +32,9 @@ def main():
             if event.type == pygame.QUIT:
                 processing = False
                 break
-    
+        
         game_window()
+
     pygame.quit()
 
 if __name__ == "__main__":
